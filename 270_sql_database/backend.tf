d24133944e849e34aee850e24eed4a9400499948
chouchene1993 <47932381+chouchene1993@users.noreply.github.com>
2022-10-27 15:44:30 +0200

    Update backend.tf

--- 270_sql_database/backend.tf
+++ 270_sql_database/backend.tf
@@ -1,12 +1,10 @@
 terraform {
   backend "azurerm" {
-<<<<<<< HEAD
-=======
-
+    
     resource_group_name  = "azrgd-rtmassbalance-01"
     storage_account_name = "azstdrtmb01"
     container_name       = "azctdrtmb03"
     key                  = "d-270_sql_database.tfstate"
->>>>>>> main
+   
   }
 }
